9954a3dcecdad95804d577d7b261d73aa2a3c4fd
gannjobs <linksssyamato@gmail.com>
2025-06-05 19:39:41 -0300

    Criação do lplano, relacionamento com o cliente, criaçao do pagamento (sem modelar)

--- Cliente/models.py
+++ Cliente/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from Plano.models import Plano
 # Create your models here.
 class Pais(models.Model):
     nome = models.CharField(max_length=100, unique=True)
@@ -38,6 +39,7 @@ class Cliente(models.Model):
     status = models.BooleanField(default=True)
     telefone = models.CharField(max_length=15, blank=True, null=True)
     endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+    plano = models.ForeignKey(Plano, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.vinculo.first_name
--- Plano/models.py
+++ Plano/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Plano(models.Model):
+    nome = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    beneficios = models.TextField()
