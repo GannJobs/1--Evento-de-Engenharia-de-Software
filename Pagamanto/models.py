f0e2808416cd83ef7df572d62f5d16aeec72e7db
gannjobs <linksssyamato@gmail.com>
2025-06-05 19:44:06 -0300

    Atualizamos a hora para nosso territorio + fizemos a classe de pagamento (forma de pagamento)

--- Pagamanto/models.py
+++ Pagamanto/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from Cliente.models import Cliente
+from Plano.models import Plano
 # Create your models here.
+class Forma_Pagamento(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
+class Pagamento(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    plano = models.ForeignKey(Plano, on_delete=models.CASCADE)
+    forma_pagamento = models.ForeignKey(Forma_Pagamento, on_delete=models.CASCADE)
+    data_criado = models.DateTimeField(auto_now_add=True)
+    nota_fiscal = models.FileField(upload_to='notas_fiscais/', blank=True, null=True)
